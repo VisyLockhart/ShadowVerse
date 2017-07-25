8c740e5369a60c2ea15d54da9812bd55c75d3eef
frohmanthenoob <cabal_357@hotmail.com>
2017-07-26 02:51:22 +0800

    增加Spell的PartialClass，避免EF更新時蓋掉Model資訊。

---
+++ ShadowVerse/ShadowVerse/Models/Spell.Partial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace ShadowVerse.Models
+{
+    [MetadataType(typeof(SpellMetaData))]
+    public partial class Spell
+    {
+    }
+    public partial class SpellMetaData
+    {
+        public int id { get; set; }
+        public string story { get; set; }
+        public string ability { get; set; }
+        public string imgUrl { get; set; }
+        [StringLength(1,ErrorMessage ="只能是一個Char")]
+        public string enhance { get; set; }
+    }
+}
